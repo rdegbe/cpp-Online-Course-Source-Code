974f33d5162fb66e7251f8b9150bcd83821c69a9
Raymond Degbe <rdegbe14@gmail.com>
2022-03-16 17:50:34 +0000

    Nothing important

--- 37.Polymorphism/37.2StaticBindingWithInheritance/oval.h
+++ 37.Polymorphism/37.2StaticBindingWithInheritance/oval.h
@@ -5,14 +5,12 @@ class Oval : public Shape
 {
 public:
     Oval()= default;
-    Oval(double x_radius, double y_radius,
-                std::string_view description);
+    Oval(double x_radius, double y_radius, std::string_view description);
     ~Oval();
     
     void draw() const{
         std::cout << "Oval::draw() called. Drawing " << m_description <<
-            " with m_x_radius : " << m_x_radius << " and m_y_radius : " << m_y_radius 
-                    << std::endl;
+            " with m_x_radius : " << m_x_radius << " and m_y_radius : " << m_y_radius << std::endl;
     }
 
 protected:
